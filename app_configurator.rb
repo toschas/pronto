62c0abb822b6f8e5aa386bfba405fbeeb33d6f5e
Aleksandar Radunovic <a.radunovic@live.com>
2015-04-24 12:18:34 +0200

    fix heredoc indentation

--- app_configurator.rb
+++ app_configurator.rb
@@ -15,41 +15,39 @@ class AppConfigurator
   end
 
   def configure
-    puts 'Creating app directories...'
-    create_app_directories
-    puts 'Creating shared directories...'
-    create_shared_directories
-    puts 'Create rails configuration files (application.yml, database.yml)'
-    create_rails_yml_files
-    puts 'Configuring Puma services...'
-    configure_puma_upstart
-    puts 'Creating Nginx configuration...'
+    # create_app_directories
+    # create_shared_directories
+    # create_rails_yml_files
+    # configure_puma_upstart
     configure_nginx
-    puts "Installing Ruby v#{ruby_version}"
-    install_ruby
+    # install_ruby
   end
 
   private
 
   def create_app_directories
+    puts 'Creating app directories...'
     [rails_app_name, client_app_name].each do |app_name|
       system(%Q{mkdir -p ~/#{app_name}})
     end
   end
 
   def create_shared_directories
+    puts 'Creating shared directories...'
     %w(tmp/sockets tmp/pids log config).each do |dir|
       system(%Q{mkdir -p ~/#{rails_app_name}/shared/#{dir}})
     end
   end
 
   def create_rails_yml_files
+    puts 'Create rails configuration files (application.yml, database.yml)'
     %w(database.yml application.yml).each do |file|
       system(%Q{touch ~/#{rails_app_name}/shared/config/#{file}})
     end
   end
 
   def configure_puma_upstart
+    puts 'Configuring Puma services...'
     set_jungle_config
     create_puma_app_file
     copy_jungle_files
@@ -75,31 +73,29 @@ class AppConfigurator
   end
 
   def configure_nginx
+    puts 'Creating Nginx configuration...'
     generate_nginx_config
     copy_nginx_config
   end
 
   def generate_nginx_config
     content = <<-EOT
-    #{puma_upstart}
+#{puma_upstart}
 
-    server {
-      listen *:80;
-      server_name #{nginx_server_name};
+server {
+  listen *:80;
+  server_name #{nginx_server_name};
 
-      root /home/#{user}/#{client_app_name}/current/#{nginx_root};
-      access_log /home/#{user}/#{rails_app_name}/current/log/nginx.access.log;
-      error_log /home/#{user}/#{rails_app_name}/current/log/nginx.error.log;
+  root /home/#{user}/#{client_app_name}/current#{nginx_root};
+  access_log /home/#{user}/#{rails_app_name}/current/log/nginx.access.log;
+  error_log /home/#{user}/#{rails_app_name}/current/log/nginx.error.log;
 
-    #{append_nginx_aliases}
-
-    #{append_root_alias}
-
-    #{append_reverse_proxies}
-
-    #{append_assets}
-    }
-    EOT
+  #{append_nginx_aliases}
+  #{append_root_alias}
+  #{append_reverse_proxies}
+  #{append_assets}
+}
+EOT
     write_file("#{ENV['HOME']}/#{nginx_server_name}", content)
   end
 
@@ -109,67 +105,68 @@ class AppConfigurator
 
   def puma_upstart
     <<-EOT
-    upstream puma {
-      server unix:///home/#{user}/#{rails_app_name}/shared/tmp/sockets/puma.sock;
-    }
-    EOT
+upstream puma {
+  server unix:///home/#{user}/#{rails_app_name}/shared/tmp/sockets/puma.sock;
+}
+EOT
   end
 
   def append_nginx_aliases
     content = ''
     nginx_aliases.each do |nginx_alias|
       content << <<-EOT
-      location #{nginx_alias[:location]} {
-        alias #{nginx_alias[:alias]};
-        gzip_static       on;
-        expires           max;
-        add_header        Cache-Control public;
-      }
-      EOT
+location #{nginx_alias[:location]} {
+    alias #{nginx_alias[:alias]};
+    gzip_static       on;
+    expires           max;
+    add_header        Cache-Control public;
+  }
+  EOT
     end
     content
   end
 
   def append_root_alias
     <<-EOT
-    location / {
-      alias /home/#{user}/#{client_app_name}/current/#{nginx_root};
-      gzip_static       on;
-      expires           max;
-      add_header        Cache-Control public;
-    }
-    EOT
+location / {
+    alias /home/#{user}/#{client_app_name}/current#{nginx_root};
+    gzip_static       on;
+    expires           max;
+    add_header        Cache-Control public;
+  }
+  EOT
   end
 
   def append_reverse_proxies
     content = ""
     nginx_proxies.each do |proxy|
       content << <<-EOT
-    location #{proxy[:location]} {
-      proxy_pass #{proxy[:proxy]};
-      proxy_set_header  X-Real-IP  $remote_addr;
-      proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header  X-Forwarded-Proto http;
-      proxy_set_header  Host $http_host;
-      proxy_redirect    off;
-      proxy_next_upstream error timeout invalid_header http_502;
-    }
-      EOT
+location #{proxy[:location]} {
+    proxy_pass #{proxy[:proxy]};
+    proxy_set_header  X-Real-IP  $remote_addr;
+    proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header  X-Forwarded-Proto http;
+    proxy_set_header  Host $http_host;
+    proxy_redirect    off;
+    proxy_next_upstream error timeout invalid_header http_502;
+  }
+  EOT
     end
     content
   end
 
   def append_assets
     <<-EOT
-    location ^~ /assets/ {
-      gzip_static on;
-      expires max;
-      add_header Cache-Control public;
-    }
-    EOT
+location ^~ /assets/ {
+    gzip_static on;
+    expires max;
+    add_header Cache-Control public;
+  }
+  EOT
   end
 
   def install_ruby
+    puts "Installing Ruby v#{ruby_version}"
     system(%Q{rbenv install #{ruby_version}})
   end
 
